90e4608aa7118189b4036e36b3dcfedc2e4c9240
dustin <dstywho@gmail.com>
2011-08-24 15:37:17 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NaivePageTracker::Application.config.secret_token = '4543cc706231240a693461f717eca104a0b4ff5486c1459d3bea68b4f3a6bab96f09a840c738e8ccace5452d3a1dede375165628c643bb3a15bd7d3190f8d507'
