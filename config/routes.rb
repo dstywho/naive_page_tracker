2cfb3a1cde09c1d4c28e283e2ad259c193c72f6d
dustin <dstywho@gmail.com>
2011-08-24 17:04:33 -0400

    route and views updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NaivePageTracker::Application.routes.draw do
+  root :to => "websites#new"
   resources :visits
 
   resources :websites do
@@ -56,7 +57,6 @@ NaivePageTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
